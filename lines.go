b0e480032a1e2fca2c5fe15f5110a66774d7e994
Jochen Hilgers <jochen.hilgers@antwerpes.de>
2014-11-06 12:16:41 +0100

    Worked on line diff handling

---
+++ lines.go
@@ -0,0 +1,98 @@
+package analyzer
+
+import (
+	"github.com/jochil/vcs"
+	"github.com/sergi/go-diff/diffmatchpatch"
+	"strings"
+	"fmt"
+)
+
+type LineSumDiff struct {
+	Added     uint32
+	Removed   uint32
+	Untouched uint32
+}
+
+func (d *LineSumDiff) Add(d2 LineSumDiff) {
+	d.Added += d2.Added
+	d.Removed += d2.Removed
+	d.Untouched += d2.Untouched
+}
+
+func (d LineSumDiff) String() string {
+	return fmt.Sprintf("Added: %d, Removed %d, Untouched: %d", d.Added, d.Removed, d.Untouched)
+}
+
+//TODO just a temporary implementation for testing/development
+func CountLines(file *vcs.File) uint32 {
+	content := file.String()
+	return uint32(strings.Count(content, "\n") + 1)
+}
+
+//calculates line diff for a developer
+func CalcLineDiff(dev *vcs.Developer) LineSumDiff {
+	lineSumDiff := LineSumDiff{0, 0, 0}
+	for _, commit := range dev.Commits {
+		lineSumDiff.Add(CalcLineDiffCommit(commit))
+	}
+	return lineSumDiff
+}
+
+//calculates line diff for a commit
+func CalcLineDiffCommit(commit *vcs.Commit) LineSumDiff {
+
+	lineSumDiff := LineSumDiff{0, 0, 0}
+
+	//this is the first commit in the vcs, so count all lines added
+	if len(commit.Parents) == 0 {
+		for _, file := range commit.Files {
+			lineSumDiff.Add(execLineDiff("", file.String()))
+		}
+	}  else {
+
+		//TODO handle merge
+		if len(commit.Parents) > 1 {
+
+		} else {
+			//get one and only parent commit
+			var parentCommit *vcs.Commit
+			for _, parentCommit = range commit.Parents {
+				break;
+			}
+
+			//--what is about deleted files?
+			for _, file := range commit.Files {
+				var from string = ""
+				if parentFile := parentCommit.FileByPath(file.Path); parentFile != nil {
+					from = parentFile.String()
+				}
+				lineSumDiff.Add(execLineDiff(from, file.String()))
+			}
+		}
+	}
+
+	return lineSumDiff
+}
+
+//executes the concrete line diff based on two string
+func execLineDiff(from string, to string) LineSumDiff {
+
+	dmp := diffmatchpatch.New()
+	diffs := dmp.DiffMain(from, to, true)
+
+	lineDiff := LineSumDiff{0, 0, 0}
+
+	//todo replace -1,0,1 with constants
+	for _, diff := range diffs {
+		switch diff.Type {
+		case -1:
+			lineDiff.Removed++
+		case 1:
+			lineDiff.Added++
+		case 0:
+			lineDiff.Untouched++
+		}
+	}
+
+	return lineDiff
+}
