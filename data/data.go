ab2ae73fb33b400e315dc1a8d147464ff15721d9
Jochen Hilgers <jochen.hilgers@gmail.com>
2014-11-30 15:01:10 +0100

    cleaned up some data structures

--- data/data.go
+++ data/data.go
@@ -1,21 +1,29 @@
 package data
 
 import (
-	"github.com/jochil/analyzer"
 	"fmt"
+	"github.com/jochil/analyzer"
+	"github.com/jochil/vcs"
 )
 
 type DevData struct {
-	Dev              string //TODO replace with pointer to vcs.Dev?
-	Commits          uint16
-	LineDiff         analyzer.LineSumDiff
+	Dev      string //TODO replace with pointer to vcs.Dev?
+	Commits  int
+	FileDiff analyzer.FileSumDiff
+	LineDiff analyzer.LineSumDiff
+}
+
+func NewDevData(dev *vcs.Developer) *DevData {
+	devData := &DevData{
+		Dev:      dev.Id,
+		Commits:  len(dev.Commits),
+		FileDiff: analyzer.CalcFileDiff(dev),
+		LineDiff: analyzer.CalcLineDiff(dev),
+	}
+	return devData
 }
 
 func (d *DevData) String() string {
-	return fmt.Sprintf(
-		"%s: \n\t Commits: %d\n\t LineDiff: %s",
-		d.Dev,
-		d.Commits,
-		d.LineDiff,
-	)
+	return fmt.Sprintf("%s\n\tCommits: %d \n\tFiles: %s \n\tLines: %s",
+		d.Dev, d.Commits, d.FileDiff, d.LineDiff)
 }
--- scabov.go
+++ scabov.go
@@ -1,11 +1,12 @@
 package main
 
 import (
-	"github.com/jochil/vcs"
-	"log"
 	"flag"
-	"io/ioutil"
 	"github.com/jochil/analyzer"
+	"github.com/jochil/scabov/data"
+	"github.com/jochil/vcs"
+	"io/ioutil"
+	"log"
 )
 
 var (
@@ -28,7 +29,6 @@ func main() {
 	vcs.Filter = filter
 	analyzer.Filter = filter
 
-
 	// load repo
 	if *repoPath == "" {
 		log.Fatal("repository path missing, e.g.: -p \"mypath/repo\"")
@@ -36,50 +36,9 @@ func main() {
 
 	repo := vcs.NewRepository(*repoPath)
 
-
-
+	//TESTING
 	log.Println("-------------")
 	for _, dev := range repo.Developers {
-		for _, commit := range dev.Commits {
-			log.Println(commit)
-			for path, file := range commit.Files {
-				log.Println("\t", path, file)
-			}
-		}
+		log.Println(data.NewDevData(dev))
 	}
-
-	/*
-	log.Println("-------------")
-	for _, dev := range repo.Developers {
-		devData := data.DevData{}
-		devData.Dev = dev.Id
-		devData.Commits = uint16(len(dev.Commits))
-		devData.LineDiff = analyzer.CalcLineDiff(dev)
-		log.Println(devData.String())
-		log.Printf("\t FileDiff: Added: %d, Removed: %d, Changed: %d",
-			len(dev.NewFiles), len(dev.RemovedFiles), len(dev.ChangedFiles),
-		)
-	}
-
-	*/
-	//firstCommit := repo.FirstCommit()
-	//log.Println("First commit", firstCommit)
-
-	/*
-
-	file1 := repo.FindFileInCommit("869f47702d5b1ec4221e4833a008551792a14632", "83a5be867e7a2355646aeaa0be1389b54bfa1c94")
-	file2 := repo.FindFileInCommit("ca564cbf4736a6f5dfeadae9e14303e0c5e1ad3d", "a2f091edd4143080b9b98493485af80de6e9dbff")
-
-	log.Println(analyzer.CountLineDiff(file1.String(), file2.String()))
-
-	repo.AllCommits()
-
-	repo.FindCommit("83a5be867e7a2355646aeaa0be1389b54bfa1c94")
-
-	file1 := repo.FindFileInCommit("869f47702d5b1ec4221e4833a008551792a14632", "83a5be867e7a2355646aeaa0be1389b54bfa1c94")
-	log.Println(string(file1.Contents[:]))
-
-	file2 := repo.FindFileInCommit("ca564cbf4736a6f5dfeadae9e14303e0c5e1ad3d", "a2f091edd4143080b9b98493485af80de6e9dbff")
-	log.Println(string(file2.Contents[:]))
-	*/
 }
