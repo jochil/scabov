37177a6a8beac97948226ce3846b18a73fd192d3
Jochen Hilgers <jochen.hilgers@antwerpes.de>
2015-01-06 17:47:02 +0100

    Worked on mojena test

--- classifier/merger.go
+++ classifier/merger.go
@@ -2,7 +2,6 @@ package classifier
 
 import (
 	"fmt"
-	"log"
 	"math"
 	"strings"
 )
@@ -44,7 +43,28 @@ const (
 	singleLinkageMode   = iota
 )
 
-func Linkage(groups []*Group, mode int) []*Group {
+type mergeStep struct {
+	level     float64
+	normLevel float64
+	groups    []*Group
+}
+
+func newMergeStep(groups []*Group, level float64) *mergeStep {
+
+	//copy groups (to avoid rearrangement through pointer)
+	copiedGroups := []*Group{}
+	for _, group := range groups {
+		newGroup := Group{Objects: group.Objects}
+		copiedGroups = append(copiedGroups, &newGroup)
+	}
+
+	return &mergeStep{
+		level:  level,
+		groups: copiedGroups,
+	}
+}
+
+func Linkage(groups []*Group, mode int) ([]*Group, *Group) {
 
 	//get the two groups with the smallest proximity
 	var minProximity float64
@@ -89,18 +109,93 @@ func Linkage(groups []*Group, mode int) []*Group {
 		delete(group.Proximites, groupB)
 	}
 
-	return newGroups
+	return newGroups, groupA
 
 }
 
-func Merge(matrix map[string]map[string]float64) {
+func Merge(matrix map[string]map[string]float64) []*Group {
 	groups := convertToGroups(matrix)
 
-	for len(groups) > 1 {
-		groups = Linkage(groups, completeLinkageMode)
-		log.Println(groups)
+	var newGroup *Group
+	steps := map[int]*mergeStep{}
+
+	steps[len(groups)] = &mergeStep{
+		level: 0.0, //TODO replace
 	}
 
+	minLevel := 10000.0  //TODO replace
+	maxLevel := -10000.0 //TODO replace
+
+	for len(groups) > 1 {
+		groups, newGroup = Linkage(groups, completeLinkageMode)
+
+		var level float64 = -10000 //TODO replace
+		for _, objectA := range newGroup.Objects {
+			for _, objectB := range newGroup.Objects {
+				if objectA == objectB {
+					continue
+				}
+				proximity := matrix[objectA][objectB]
+				if proximity > level {
+					level = proximity
+				}
+			}
+		}
+
+		if level > maxLevel {
+			maxLevel = level
+		}
+		if level < minLevel {
+			minLevel = level
+		}
+
+		step := newMergeStep(groups, level)
+		steps[len(groups)] = step
+	}
+
+	//rescale levels to 1-7
+	for _, step := range steps {
+		step.normLevel = 1 + (step.level-minLevel)*(7-1)/(maxLevel-minLevel)
+		//log.Println(len(step.groups), step.level, step.normLevel)
+	}
+
+	clusterCount := testMojena(steps)
+	return steps[clusterCount].groups
+}
+
+func testMojena(steps map[int]*mergeStep) int {
+
+	for k := len(steps) - 1; k > 1; k-- {
+		sum := 0.0
+		for i := len(steps) - 1; i >= k; i-- {
+			sum += steps[i].normLevel
+		}
+		vk := sum / float64(len(steps)-k)
+
+		sum = 0.0
+		for i := len(steps) - 1; i >= k; i-- {
+			sum += math.Pow(steps[i].normLevel-vk, 2)
+		}
+		sk := math.Sqrt(sum * 1.0 / (float64(len(steps)-k) - 1.0))
+
+		next := steps[k-1].normLevel
+		test := (next - vk) / sk
+
+		if math.IsNaN(test) == false && test > 2.75 {
+			return k
+		}
+
+		/*log.Println("step", len(steps)-k)
+		log.Println("cluster", k)
+		log.Println("durchschnitt", vk)
+		log.Println("standardabweichung", sk)
+		log.Println("vk+1", next)
+		log.Println("test", test)
+		log.Println("---------")*/
+
+	}
+
+	return 0
 }
 
 //convert a matrix of objects into a 'one group one object' structure
